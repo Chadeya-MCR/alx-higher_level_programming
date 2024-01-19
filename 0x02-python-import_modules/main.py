35f5af080394b4c70d91fa0a371fcba6f78c139c
Chadeya-MCR <chadeyamorgan203@gmail.com>
2024-01-19 05:35:59 +0300

    program that imports the function def add(a, b): from the file add_0.py:

---
+++ 0x02-python-import_modules/main.py
@@ -0,0 +1,12 @@
+# main.py
+
+# Assign values to variables a and b
+a = 1
+b = 2
+
+# Import the add function from add_0.py
+from add_0 import add
+
+# Print the result using string format
+print("{} + {} = {}".format(a, b, add(a, b)))
+
