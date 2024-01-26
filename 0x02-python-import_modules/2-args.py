d1cc1a3391cfec8721bce85aeeabed9494e1e587
Chadeya-MCR <chadeyamorgan203@gmail.com>
2024-01-26 12:21:31 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 
-# Get the number of arguments passed
-num_args = len(argv) - 1
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
 
-# Print the number of arguments passed to the sript
-print("Number of argument{}: {}{}".format('' if num_args == 1 else 's', num_args, '' if num_args != 0 else '.'))
-
-# Print the list of arguments passed to the script
-if num_args > 0:
-    print("Argument{}:".format('s' if num_args > 1 else ''))
-    
-    # Iterate through the arguments and print each of them
-    for i in range(1, num_args + 1):
-        print("{}: {}".format(i, argv[i]))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
