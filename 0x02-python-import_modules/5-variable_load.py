101d9492e0edb964e220e8c1ffab97da629bb1b3
Chadeya-MCR <chadeyamorgan203@gmail.com>
2024-01-26 12:34:15 +0300

    correction

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,6 @@
-# main file
-
-# Import the variable 'a' from variable_load_5.py
+#!/usr/bin/python3
 from variable_load_5 import a
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
 
-# Print the value of 'a'
-print(a)
-
+    print(a)
