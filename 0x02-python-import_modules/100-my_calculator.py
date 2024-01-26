26a808ebd9ae8dc30abbbb435a1dfdecd3c155ac
Chadeya-MCR <chadeyamorgan203@gmail.com>
2024-01-26 12:57:10 +0300

    Correction

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-from sys import argv
 from calculator_1 import add, sub, mul, div
+import sys
 
-if len(argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
+if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
 
-a = int(argv[1])
-operator = argv[2]
-b = int(argv[3])
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-if operator == '+':
-    result = add(a, b)
-elif operator == '-':
-    result = sub(a, b)
-elif operator == '*':
-    result = mul(a, b)
-elif operator == '/':
-    result = div(a, b)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-print("{} {} {} = {}".format(a, operator, b, result))
-
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,15 @@
-def magic_calculation(a, b):
-    add = __import__('magic_calculation_102').add
-    sub = __import__('magic_calculation_102').sub
+#!/usr/bin/python3
+from magic_calculation_102 import add, sub
 
+
+def magic_calculation(a, b):
+
+    """Match bytecode provided by Holberton School."""
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return c
+        return (c)
+
     else:
-        return sub(a, b)
+        return (sub(a, b))
