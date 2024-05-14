53121540c2a22bbcbbbddfdb0fe2d7788cca0764
Chadeya-MCR <chadeyamorgan203@gmail.com>
2024-05-15 00:30:23 +0300

    Fetching url resources using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,14 @@
+#!/usr/bin/python3
 import urllib.request
 
 url = 'https://alx-intranet.hbtn.io/status'
 
 with urllib.request.urlopen(url) as response:
-    body = response.read().decode('utf-8')
+    html = response.read()
+    utf8_content = html.decode('utf-8')
 
-print("- type: {}".format(type(body)))
-print("- content: {}".format(body))
+print("Body response:")
+print("\t- type:", type(html))
+print("\t- content:", html)
+print("\t- utf8 content:", utf8_content)
 
